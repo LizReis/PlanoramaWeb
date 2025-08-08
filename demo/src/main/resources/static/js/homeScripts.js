83980c855f9e9f8aca991dbb5a8c5f3464eff3a5
Liz <124801714+LizReis@users.noreply.github.com>
2025-08-08 16:35:18 +0000

    Adicionado 8 telas do projeto (Estão em formato para mobile ainda)

---
+++ demo/src/main/resources/static/js/homeScripts.js
@@ -0,0 +1,7 @@
+//SCRIPT PARA A BARRA DE PROGRESSO CRESCER (LA ELE) COM
+//BASE NO PROGRESSO TOTAL DA SOMA DOS PLANOS QUE ELE ESTÁ FAZENDO
+
+let progresso = 0;
+let barraProgresso = document.getElementById("progresso");
+
+barraProgresso.addEventListener()
